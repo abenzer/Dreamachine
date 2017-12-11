bb9c6a6d62dd98c93fa46319e3a35d64f551212c
Alex Benzer <abenzer@gmail.com>
2017-12-10 23:22:24 -0800

    Initial commit

---
+++ Assets/OVR/Scripts/Alex Scripts/lightStrobe.js
@@ -0,0 +1,23 @@
+﻿// Interpolate light color between two colors back and forth
+var duration : float = 20.0;
+var color0 : Color = Color.red;
+var color1 : Color = Color.blue;
+var color2 : Color = Color.black;
+var counter : boolean = false;
+
+var lt: Light;
+
+function Start () {
+    lt = GetComponent.<Light>();
+}
+
+function Update () {
+    var t : float = Mathf.PingPong(Time.time, duration) / duration;
+    // lt.color = Color.Lerp(color0, color1, t);
+    if(counter) {
+    	lt.color = color0;
+	} else {
+		lt.color = color1;
+	}
+	counter = !counter;
+}
---
+++ Assets/OVR/Scripts/Alex Scripts/setFPS.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+function Start () {
+    // Make the game run as fast as possible
+    Application.targetFrameRate = 5;
+}
+
+function Update () {
+	
+}
+
+function Awake () {
+    // Make the game run as fast as possible
+    Application.targetFrameRate = 5;
+}
