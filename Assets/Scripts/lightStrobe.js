20ca8e3ed63802de58a2d0465370faab8c2f1ca0
Alex Benzer <abenzer@gmail.com>
2017-12-11 00:27:38 -0800

    Head tracking with color

--- Assets/Scripts/lightStrobe.js
+++ Assets/Scripts/lightStrobe.js
@@ -1,37 +1,50 @@
-﻿// Interpolate light color between two colors back and forth
-var duration : float = 20.0;
+﻿var duration : float = 20.0;
+var colorStart0 : Color = Color.red;
+var colorStart1 : Color = Color.blue;
 var color0 : Color = Color.red;
 var color1 : Color = Color.blue;
 var counter : boolean = false;
 var canCallFunction: boolean = true;
 var lt : Light;
 
-/* config */
+//config
 var configSpeed : float = .025;  
 
 
-/* run on start */
+// run on start
 function Start () {
     lt = GetComponent.<Light>();
 }
 
-/* run every frame */
+// run every frame
 function Update () {
    if(canCallFunction) {
    	  UpdateColors();
       Wait(configSpeed);
    }
 
+   // get camera position
+   var camX : float = Camera.main.transform.eulerAngles.x;
+   var camY : float = Camera.main.transform.eulerAngles.y;
+   var camZ : float = Camera.main.transform.eulerAngles.z;
+   Debug.Log("x: "+camX+"... y: "+camY+"... z:"+camZ);
+
+   // update colors based on x position
+   //color0 = Color();
+   color0 = Color(camX / 5000, camY / 5000, camZ / 5000, 1); 
+
+   // update speed based on y position
+   configSpeed = camY / 5000;
 }
 
-/* delay */
+// delay
 function Wait(time : float) {
 	canCallFunction= false;
 	yield new WaitForSeconds(time);
 	canCallFunction= true;
 }
   
-/* update colors */
+// update colors 
 function UpdateColors() {
     var t : float = Mathf.PingPong(Time.time, duration) / duration;
     // lt.color = Color.Lerp(color0, color1, t);
